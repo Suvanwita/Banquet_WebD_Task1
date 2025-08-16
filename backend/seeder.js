af9d866ef0900ff0cf00610617f44132a6d6d230
Suvanwita <dsuvanwita@gmail.com>
2025-08-16 22:57:38 +0530

    Added seeder.js

---
+++ backend/seeder.js
@@ -0,0 +1,37 @@
+const mongoose=require('mongoose')
+const dotenv=require('dotenv')
+const axios=require('axios')
+
+const {User}=require('./models/User')
+const {Question}=require('./models/Question')
+const {Category}=require('./models/Category')
+
+dotenv.config()
+
+const connectDB=require('./config/db')
+
+const seedData=async()=>{
+    try{
+        const {data}=await axios.get('https://test-data-gules.vercel.app/data.json')
+        await Question.deleteMany();
+        await Category.deleteMany();
+
+        const allQuestions=data.data.flatMap((category)=>category.ques);
+        const insertedQuestions=await Question.insertMany(allQuestions);
+
+        const categories=data.data.map((category)=>({
+            title:category.title,
+            questions: insertedQuestions
+                        .filter((q)=>category.ques.some((cq)=>cq.title===q.title))
+                        .map((q)=>q._id),
+        }))
+
+        await Category.insertMany(categories);
+    }
+    catch(err){
+        console.log("Seeding error: ",err.message)
+    }
+}
+
+connectDB().then(seedData)
+
