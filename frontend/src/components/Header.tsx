e53235d1084092fa79bf8f0f91f9a5468a667ffb
Suvanwita <dsuvanwita@gmail.com>
2025-08-17 23:29:01 +0530

    Added frontend code

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,17 @@
+import "./Header.css";
+
+export default function Header(){
+    return(
+        <div className="header">
+            <p>
+                Banquet
+            </p>
+            <p>
+                Home
+            </p>
+            <button>
+                Sign In
+            </button>
+        </div>
+    )
+}
